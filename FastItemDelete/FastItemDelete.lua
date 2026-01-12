88d3d35b8d6c49d8aaf6c756b5306b60f11b8211
BloodDragon2580 <gewinner.thomas@gmail.com>
2026-01-12 10:23:09 +0100

    'Update'

--- FastItemDelete/FastItemDelete.lua
+++ FastItemDelete/FastItemDelete.lua
@@ -1,32 +1,85 @@
-local folder, ns = ...
-local addon = CreateFrame('Frame','FastItemDeleteFrame')
+-- FastItemDelete (Retail: The War Within / future-proof for Midnight)
+local addonName = ...
+local f = CreateFrame("Frame", "FastItemDeleteFrame")
 
-function addon:DELETE_ITEM_CONFIRM(...)
-    if StaticPopup1EditBox:IsShown() then
-        StaticPopup1EditBox:Hide()
-        StaticPopup1Button1:Enable()
+-- Which static popup dialogs are the "type DELETE to confirm" ones
+local DESTROY_DIALOGS = {
+  DELETE_ITEM = true,
+  DELETE_GOOD_ITEM = true,
+  DELETE_QUEST_ITEM = true,
+  DELETE_GOOD_QUEST_ITEM = true,
+}
 
-        local link = select(3,GetCursorInfo())
+local function GetCursorItemLinkFallback(itemName)
+  local infoType, _, itemLink = GetCursorInfo()
+  if infoType == "item" and itemLink then
+    return itemLink
+  end
+  return itemName -- fallback (at least show something)
+end
 
-        addon.link:SetText(link)
-        addon.link:Show()
+local function EnsureLinkFontString(popup)
+  if popup.__FastItemDeleteLink then return popup.__FastItemDeleteLink end
+
+  local fs = popup:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
+  fs:SetJustifyH("CENTER")
+  fs:SetJustifyV("MIDDLE")
+  fs:Hide()
+
+  -- Anchor roughly where the edit box sits (works across popup instances)
+  local editBox = popup.editBox or _G[popup:GetName() .. "EditBox"]
+  if editBox then
+    fs:SetPoint("CENTER", editBox, "CENTER", 0, 0)
+  else
+    fs:SetPoint("CENTER", popup, "CENTER", 0, -10)
+  end
+
+  popup:HookScript("OnHide", function(self)
+    if self.__FastItemDeleteLink then
+      self.__FastItemDeleteLink:Hide()
     end
+  end)
+
+  popup.__FastItemDeleteLink = fs
+  return fs
 end
 
-function addon:ADDON_LOADED(loaded_addon)
-    if loaded_addon ~= folder then return end
-    addon.link = StaticPopup1:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
-    addon.link:SetPoint('CENTER', StaticPopup1EditBox)
-    addon.link:Hide()
+function f:DELETE_ITEM_CONFIRM(itemName)
+  -- Search the visible static popup(s). Retail has up to 4 concurrent dialogs.
+  for i = 1, 4 do
+    local popup = _G["StaticPopup" .. i]
+    if popup and popup:IsShown() and DESTROY_DIALOGS[popup.which] then
+      local editBox = popup.editBox or _G["StaticPopup" .. i .. "EditBox"]
+      if editBox and editBox:IsShown() then
+        editBox:Hide()
+      end
 
-    StaticPopup1:HookScript('OnHide', function(self)
-        addon.link:Hide()
-    end)
+      -- Make sure the accept button is clickable
+      if popup.button1 then
+        popup.button1:Enable()
+      elseif _G["StaticPopup" .. i .. "Button1"] then
+        _G["StaticPopup" .. i .. "Button1"]:Enable()
+      end
+
+      -- Show the item link (or name if link isn't available)
+      local fs = EnsureLinkFontString(popup)
+      fs:SetText(GetCursorItemLinkFallback(itemName))
+      fs:Show()
+
+      -- We handled the active destroy popup; stop here.
+      return
+    end
+  end
 end
 
-addon:SetScript('OnEvent', function(self,event,...)
-    self[event](self,...)
+function f:ADDON_LOADED(name)
+  if name ~= addonName then return end
+  -- Nothing required here, but keeping the event for clean startup.
+end
+
+f:SetScript("OnEvent", function(self, event, ...)
+  if self[event] then self[event](self, ...) end
 end)
 
-addon:RegisterEvent('ADDON_LOADED')
-addon:RegisterEvent('DELETE_ITEM_CONFIRM')
+f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("DELETE_ITEM_CONFIRM")
